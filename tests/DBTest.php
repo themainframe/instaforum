0b016d7d845a6425bfbb109a3f0bb4d9309e4fcc
Damien Walsh <me@damow.net>
2012-11-15 00:02:38 +0000

    Added unit test stub for DB class.

---
+++ tests/DBTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Defines the Database engine test class.
+ *
+ * @author Damien Walsh <walshd0@cs.man.ac.uk>
+ */
+
+// Load required classes
+include 'classes/datasource/Files.class.php';
+include 'classes/datasource/DB.class.php';
+include 'classes/datasource/Predicate.class.php';
+include 'classes/datasource/Result.class.php';
+
+/**
+ * DB Test class
+ * Evaluates the DB class.
+ */
+class DBTest
+{
+
+} 
