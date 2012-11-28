e571e5bbe36c4c76e711a468ba03f09728bb23bf
Damien Walsh <m4infr4me@gmail.com>
2012-11-28 04:26:01 +0000

    Rudimentary checking of init process.

--- index.php
+++ index.php
@@ -6,3 +6,16 @@
  * @author Damien Walsh <walshd0@cs.man.ac.uk>
  */
 
+// --------------------------------------------------
+// Include required files
+// --------------------------------------------------
+require_once 'init.php';
+require_once IF_ROOT_PATH . '/classes/IF_Kernel.class.php';
+
+// --------------------------------------------------
+// Initialise
+// --------------------------------------------------
+if(!IF_Kernel::init())
+{
+  print '<strong>Problems encountered during kernel init.</strong>';
+}
