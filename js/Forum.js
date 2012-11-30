2d16fb5fe36cf9a6875188e3559d49d6bf2f4434
Damien Walsh <m4infr4me@gmail.com>
2012-11-30 00:51:28 +0000

    Simplified structure

--- js/Forum.js
+++ js/Forum.js
@@ -5,3 +5,52 @@
  * @author Damien Walsh <walshd0@cs.man.ac.uk>
  */
 
+// --------------------------------------------------
+// Init the forum on load
+// --------------------------------------------------
+
+// "Smallest DOMready ever" - from http://dustindiaz.com/smallest-domready-ever
+function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+
+// Run when ready
+r(function() { Forum.init() });
+
+
+// --------------------------------------------------
+// The Forum object: provides interaction with IF.
+// --------------------------------------------------
+var Forum = {
+
+  /**
+   * Values required by IF.
+   */
+  'vars' : {
+
+  },
+
+  /**
+   * Cache object
+   * Implements caching of vars in cookies.
+   */
+  'cache' : {
+
+  },
+
+  /**
+   * Initialise the forum
+   */
+  'init' : function() 
+  {
+    // Retrieve vars from cache if possible
+    this.retrieve_cached_vars();
+  },
+
+  /**
+   * Retrieve vars from the cache / cookies
+   */
+  'retrieve_cached_vars' : function()
+  {
+
+  }
+
+};
