68ea391780c1f2daf824b50cfb6b4eecc9d4215a
Damien Walsh <m4infr4me@gmail.com>
2012-12-06 01:40:50 +0000

    Added a stub method for getTitle

---
+++ classes/modules/Board.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Board.class.php
+ * Defines the Board module
+ *   
+ * @author Damien Walsh <walshd0@cs.man.ac.uk>
+ */
+/**
+ * The Board module class.
+ *
+ * The Board module provides access to the main board parameters and
+ * operations.
+ *
+ * @package IF
+ */
+class Board
+{
+  /**
+   * Get the title of the board.
+   * 
+   * @return string
+   */
+  public function getTitle()
+  {
+    return 'The board title';
+  }
+}
