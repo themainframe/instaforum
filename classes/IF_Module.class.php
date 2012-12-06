8170542b3ff5d4de6334297a644aa1e57f4e8594
Damien Walsh <m4infr4me@gmail.com>
2012-12-06 03:00:39 +0000

    Added abstract module base class

---
+++ classes/IF_Module.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * IF_Module.class.php
+ * Defines the base module class.
+ *   
+ * @author Damien Walsh <walshd0@cs.man.ac.uk>
+ */
+/**
+ * The base module class.
+ * Abstract.
+ *
+ * @package IF
+ */
+abstract class IF_Module
+{
+  /** 
+   * A reference to the kernel class that created this object.
+   * 
+   * @var *IF_Kernel
+   */
+  protected $parent = null;
+}
