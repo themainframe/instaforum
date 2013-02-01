20fb3ac0b6f0036ce8a6f53991333c91b2413bfa
Damien Walsh <m4infr4me@gmail.com>
2013-02-01 12:44:15 +0000

    Implement data class

--- acp_views/forums.acp.php
+++ acp_views/forums.acp.php
@@ -3,17 +3,11 @@
     <p>This section of the Admin Panel allows you to control the
     discussion forums that are available on the board.</p>
 
-    <table>
-      <thead>
-        <tr>
-          <td>&nbsp;</td>
-          <td>Name</td>
-          <td>Topics</td>
-          <td>Posts</td>
-          <td>Options</td>
-        </tr>
-      </thead>
-      <tbody>
+    <?php
 
-      </tbody>
-    </table>
+      $data = new IF_Dataview();
+      $data->columns = array(
+        'id' => array('name' => '')
+      );
+
+    ?>
