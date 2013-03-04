5b936ca488a7d03c147918968691178762963f03
Damien Walsh <m4infr4me@gmail.com>
2013-03-04 13:11:05 +0000

    Add user module class

---
+++ classes/modules/IF_Module_User.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * IF_Module_User.class.php
+ * Defines the User module class.
+ *   
+ * @author Damien Walsh <walshd0@cs.man.ac.uk>
+ */
+/**
+ * The User module class.
+ *
+ * The User module provides access to permissions and user account
+ * alterations.
+ *
+ * @package IF
+ */
+class IF_Module_user extends IF_Module
+{
+}
