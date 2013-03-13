9838041d532d6fac2949239f2f3750fa0d734b26
Damien Walsh <m4infr4me@gmail.com>
2013-03-13 13:52:48 +0000

    Added something to fill out the home screen.

--- acp_views/acp/home.acp.php
+++ acp_views/acp/home.acp.php
@@ -15,9 +15,18 @@ if(!defined('IF_IN_ACP'))
 }
 
 ?>
-    <h1>Welcome</h1>
+    <h1>Welcome to <?php print IF_APP_NAME; ?></h1>
     <p>
-      
-     
+
+      <strong><?php print IF_APP_NAME; ?></strong> is an instant-deployment fully-integrated web forum solution written in PHP &amp; JS.<br />
+      <br />
+
+      It comprises a storage engine, forum engine &amp; administration zone in an easy-to-install package.
+      <br />
+      <?php print IF_APP_NAME; ?> is a dissertation project by Damien Walsh, a third year undergraduate studying 
+      Internet Computing at the University of Manchester School of Computer Science.
+
+      <br /><br />
+      <a href="http://github.com/themainframe/instaforum">Click here</a> to visit the GitHub page for the project.
 
     </p>
